b45fd36805e70c00fc7f94ed3c0524484e19c180
thgilciffart <wendyscashier1@gmail.com>
2025-12-21 15:37:57 +1100

    remove dwm flexipatch

--- install.sh
+++ install.sh
@@ -1,50 +1,7 @@
 #!/bin/sh
-cat <<'PENGUIN'
-        .--.
-       |o_o |
-       |:_/ |
-      //   \ \
-     (|     | )
-    /'\_   _/`\
-    \___)=(___/
-PENGUIN
+sudo pacman -S yay
 
-echo "Welcome to the system installer!"
-echo "User: $USER"
-echo "Date: $(date '+%Y-%m-%d %H:%M:%S')"
-
-# Install yay
-
-cat <<'yay-installer'
-_____          _        _ _ _              __   __
-|_   _|        | |      | | (_)             \ \ / /
- | | _ __  ___| |_ __ _| | |_ _ __   __ _   \ V /__ _ _   _
- | || '_ \/ __| __/ _` | | | | '_ \ / _` |   \ // _` | | | |
-_| || | | \__ \ || (_| | | | | | | | (_| |   | | (_| | |_| |
-\___/_| |_|___/\__\__,_|_|_|_|_| |_|\__, |   \_/\__,_|\__, |
-                                     __/ |             __/ |
-                                    |___/             |___/
-yay-installer
-
-./scripts/yay.sh
-
-cat <<'package-installer'
-_____          _        _ _ _                                _
-|_   _|        | |      | | (_)                              | |
- | | _ __  ___| |_ __ _| | |_ _ __   __ _   _ __   __ _  ___| | ____ _  __ _  ___  ___
- | || '_ \/ __| __/ _` | | | | '_ \ / _` | | '_ \ / _` |/ __| |/ / _` |/ _` |/ _ \/ __|
-_| || | | \__ \ || (_| | | | | | | | (_| | | |_) | (_| | (__|   < (_| | (_| |  __/\__ \
-\___/_| |_|___/\__\__,_|_|_|_|_| |_|\__, | | .__/ \__,_|\___|_|\_\__,_|\__, |\___||___/
-                                     __/ | | |                          __/ |
-                                    |___/  |_|                         |___/
-package-installer
-
-yay -S --needed --noconfirm - <packages/development.txt
-yay -S --needed --noconfirm - <packages/system.txt
-yay -S --needed --noconfirm - <packages/wm.txt
-yay -S --needed --noconfirm - <packages/productivity.txt
-yay -S --needed --noconfirm - <packages/entertainment.txt
-yay -S --needed --noconfirm - <packages/misc.txt
+yay -S
 
 cp -r .config $HOME
 cp -r ./configurations/texmf $HOME
