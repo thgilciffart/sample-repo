530cec1c17ecc50ec4a0ca882db9390a488327be
thgilciffart <wendyscashier1@gmail.com>
2025-12-30 18:22:13 +1100

    \usepackage{booktabs}

--- install.sh
+++ install.sh
@@ -3,4 +3,14 @@
 sudo pacman -S yay
 
 # Install universal packages
-yay -S -needed - <./devices/universal-packages.txt
+sudo pacman -S --needed - <./devices/universal-packages.txt
+
+# Syncthing configuration
+# Load up default syncthing configuration
+
+syncthing -no-browser -no-restart >/dev/null 2>&1 </dev/null &
+sleep 10
+pkill syncthing
+sed -i 's/127.0.0.1/0.0.0.0/' $HOME/.local/state/syncthing/config.xml
+sudo ufw allow syncthing
+sudo ufw allow syncthing-gui
