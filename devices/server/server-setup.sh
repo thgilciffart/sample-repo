8efe7d4e2138574377938344364cb3490092e890
thgilciffart <wendyscashier1@gmail.com>
2026-01-08 15:50:02 +1100

    remove dwm flexipatch

--- devices/server/server-setup.sh
+++ devices/server/server-setup.sh
@@ -28,15 +28,8 @@ read -p "Enter Overleaf App Name (defaults to Overleaf Community Extended): " in
 APP_NAME=${input_appname:-Overleaf Community Extended}
 read -p "Enter Overleaf Port (defaults to 4000): " input_port
 PORT=${input_port:-4000}
-read -p "Expose Overleaf to network? (defaults to no): " expose_choice
-case "${expose_choice:-n}" in
-[yY]*)
-  LISTEN_IP="0.0.0.0"
-  ;;
-*)
-  LISTEN_IP="127.0.0.1"
-  ;;
-esac
+read -p "Enter Overleaf listening IP (defaults to 127.0.0.1): " input_listening_ip
+LISTEN_IP=${input_listening_ip:-127.0.0.1}
 read -p "Enter Overleaf site url: " input_url
 SITE_URL=${input_url:-localhost://}
 
