124c77253a293ce554bd95e1f520f31d05edb3cd
thgilciffart <wendyscashier1@gmail.com>
2026-01-08 20:29:46 +1100

    Added Karakeep and Overleaf, cleaned up variable names and automatic port opening

--- devices/server/server-setup.sh
+++ devices/server/server-setup.sh
@@ -14,21 +14,22 @@ sudo systemctl enable --now containerd.service
 mkdir -p "$HOME/karakeep"
 (cd "$HOME/karakeep" && wget https://raw.githubusercontent.com/karakeep-app/karakeep/main/docker/docker-compose.yml)
 read -p "Enter NEXTAUTH secret (defaults to random string): " nextauth_string
-NEXT_AUTH_SECRET_VAR=${nextauth_string:-$(openssl rand -base64 36)}
+KARAKEEP_NEXTAUTH_SECRET=${nextauth_string:-$(openssl rand -base64 36)}
 read -p "Enter Meili key (defaults to random string): " meilikey_string
-MEILI_KEY_VAR=${meilikey_string:-$(openssl rand -base64 36)}
+KARAKEEP_MEILI_KEY=${meilikey_string:-$(openssl rand -base64 36)}
 read -p "Enter the port to listen on (default 3000): " port_input
-PORT_VAR=${port_input:-3000}
+KARAKEEP_PORT=${port_input:-3000}
 read -p "Enter the listening URL (default 127.0.0.1): " url_input
-URL_VAR=${url_input:-127.0.0.1}
+KARAKEEP_URL=${url_input:-127.0.0.1}
 cat > "$HOME/karakeep/.env" <<karakeep-env
 KARAKEEP_VERSION=release
-NEXTAUTH_SECRET=$NEXT_AUTH_SECRET_VAR
-MEILI_MASTER_KEY=$MEILI_KEY_VAR
-NEXTAUTH_URL=http://$URL_VAR:$PORT_VAR
+NEXTAUTH_SECRET=$NEXTAUTH_SECRET
+MEILI_MASTER_KEY=$KARAKEEP_MEILI_KEY
+NEXTAUTH_URL=http://$KARAKEEP_URL:$KARAKEEP_PORT
 karakeep-env
-echo "This is your NextAuth Secret: $NEXT_AUTH_SECRET_VAR, copy it down."
-echo "This is your Meili Master Key: $MEILI_KEY_VAR, copy it down."
+sudo ufw allow $KARAKEEP_PORT/tcp
+echo "This is your NextAuth Secret: $KARAKEEP_NEXTAUTH_SECRET, copy it down."
+echo "This is your Meili Master Key: $KARAKEEP_MEILI_KEY, copy it down."
 (cd $HOME/karakeep && docker compose up -d)
 
 # Copyparty
@@ -42,24 +43,24 @@ $HOME/overleaf/bin/init
 
 # asking users to configure
 read -p "Enter Overleaf Project Name (defaults to overleaf-extended): " input_project
-PROJECT_NAME=${input_project:-overleaf-extended}
+OVERLEAF_PROJECT_NAME=${input_project:-overleaf-extended}
 read -p "Enter Overleaf App Name (defaults to Overleaf Community Extended): " input_appname
-APP_NAME=${input_appname:-Overleaf Community Extended}
+OVERLEAF_APP_NAME=${input_appname:-Overleaf Community Extended}
 read -p "Enter Overleaf Port (defaults to 4000): " input_port
-PORT=${input_port:-4000}
+OVERLEAF_PORT=${input_port:-4000}
 read -p "Enter Overleaf listening IP (defaults to 127.0.0.1): " input_listening_ip
-LISTEN_IP=${input_listening_ip:-127.0.0.1}
+OVERLEAF_LISTEN_IP=${input_listening_ip:-127.0.0.1}
 read -p "Enter Overleaf site url: " input_url
-SITE_URL=${input_url:-localhost://}
+OVERLEAF_SITE_URL=${input_url:-localhost://}
 
 cat >$HOME/overleaf/config/overleaf.rc <<overleaf-config # overleaf.rc config
 #### Overleaf RC ####
 
-PROJECT_NAME=$PROJECT_NAME
+PROJECT_NAME=$OVERLEAF_PROJECT_NAME
 OVERLEAF_DATA_PATH=data/overleaf
 SERVER_PRO=false
-OVERLEAF_LISTEN_IP=$LISTEN_IP
-OVERLEAF_PORT=$PORT
+OVERLEAF_LISTEN_IP=$OVERLEAF_LISTEN_IP
+OVERLEAF_PORT=$OVERLEAF_PORT
 
 SIBLING_CONTAINERS_ENABLED=false
 DOCKER_SOCKET_PATH=/var/run/docker.sock
@@ -79,8 +80,8 @@ GIT_BRIDGE_ENABLED=false
 NGINX_ENABLED=false
 overleaf-config
 cat >$HOME/overleaf/config/variables.env <<variables # variables.env config
-OVERLEAF_APP_NAME="$APP_NAME"
-OVERLEAF_SITE_URL=$SITE_URL
+OVERLEAF_APP_NAME="$OVERLEAF_APP_NAME"
+OVERLEAF_SITE_URL=$OVERLEAF_SITE_URL
 
 ENABLED_LINKED_FILE_TYPES=project_file,project_output_file
 
@@ -98,6 +99,7 @@ services:
   sharelatex:
     image: overleafcep/sharelatex:6.0.1-ext-v3.3
 docker-override
+sudo ufw allow $OVERLEAF_PORT/tcp
 $HOME/overleaf/bin/up -d
 # syncthing
 
