13590404722898989d08c336f12dbaa742abea0d
thgilciffart <wendyscashier1@gmail.com>
2026-01-14 21:25:41 +1100

    I don't know what to put as a commit message my laptop is on 3%

--- devices/server/server-setup.sh
+++ devices/server/server-setup.sh
@@ -10,26 +10,45 @@ sudo usermod -aG docker $USER
 sudo systemctl enable --now docker.service
 sudo systemctl enable --now containerd.service
 
-# Cloudflare tunnels
-
-# Tailscale
-
-# Authentik Set up
-if [ -d "$HOME/authentik" ]; then
+# nginx proxy manager
+if [ -d "$HOME/nginx-proxy-manager" ]; then
     echo "Directory exists. Moving..."
-    mv "$HOME/authentik" "$HOME/authentik.old"
-    mkdir -p "$HOME/authentik"
+    mv "$HOME/nginx-proxy-manager" "$HOME/nginx-proxy-manager.old"
+    mkdir -p "$HOME/nginx-proxy-manager"
 else
     echo "Directory does not exist. Creating..."
-    mkdir -p "$HOME/authentik"
+    mkdir -p "$HOME/nginx-proxy-manager"
 fi
 (
-    cd $HOME/authentik
-    wget https://docs.goauthentik.io/docker-compose.yml
-    echo "PG_PASS=$(openssl rand -base64 36 | tr -d '\n')" >> .env
-    echo "AUTHENTIK_SECRET_KEY=$(openssl rand -base64 60 | tr -d '\n')" >> .env
-    docker compose pull
-    docker compose up -d
+    cd $HOME/nginx-proxy-manager
+    read -p "Enter web UI port (defaults to 81): " nginx_ui_port_input
+    NGINX_UI_PORT=${nginx_ui_port_input:-81}
+    read -p "Enter time zone https://en.wikipedia.org/wiki/List_of_tz_database_time_zones (defaults to Australia/Sydney): " nginx_timezone_input
+    NGINX_TIMEZONE=${nginx_timezone_input:-Australia/Sydney}
+    read -p "Disable IPv6? (true/false): " nginx_ipv6_input
+    NGINX_IPV6=${nginx_ipv6_input:-true}
+    cat > docker-compose.yml <<nginx-config
+services:
+    app:
+        image: 'jc21/nginx-proxy-manager:latest'
+        restart: unless-stopped
+
+        ports:
+          - '80:80' # Public HTTP Port
+          - '443:443' # Public HTTPS Port
+          - '$NGINX_UI_PORT:81' # Admin Web Port
+
+        environment:
+            TZ: "$NGINX_TIMEZONE"
+            DISABLE_IPV6: '$NGINX_IPV6'
+
+        volumes:
+            - ./data:/data
+            - ./letsencrypt:/etc/letsencrypt
+nginx-config
+    sudo ufw allow $NGINX_UI_PORT
+    sudo ufw allow 443/tcp
+    sudo ufw allow 80/tcp
 )
 
 # Karakeep
@@ -46,14 +65,14 @@ fi
     cd $HOME/karakeep
     wget https://raw.githubusercontent.com/karakeep-app/karakeep/main/docker/docker-compose.yml
 
-    read -p "Enter NEXTAUTH secret (defaults to random string): " nextauth_string
-    KARAKEEP_NEXTAUTH_SECRET=${nextauth_string:-$(openssl rand -base64 36)}
-    read -p "Enter Meili key (defaults to random string): " meilikey_string
-    KARAKEEP_MEILI_KEY=${meilikey_string:-$(openssl rand -base64 36)}
-    read -p "Enter the port to listen on (default 3000): " port_input
-    KARAKEEP_PORT=${port_input:-3000}
-    read -p "Enter the listening URL (default 127.0.0.1): " url_input
-    KARAKEEP_URL=${url_input:-127.0.0.1}
+    read -p "Enter NEXTAUTH secret (defaults to random string): " nextauth_string_input
+    KARAKEEP_NEXTAUTH_SECRET=${nextauth_string_input:-$(openssl rand -base64 36)}
+    read -p "Enter Meili key (defaults to random string): " meilikey_string_input
+    KARAKEEP_MEILI_KEY=${meilikey_string_input:-$(openssl rand -base64 36)}
+    read -p "Enter the port to listen on (default 1000): " karakeep_port_input
+    KARAKEEP_PORT=${karakeep_port_input:-1000}
+    read -p "Enter the listening URL (default 0.0.0.0): " karakeep_url_input
+    KARAKEEP_URL=${karakeep_url_input:-0.0.0.0}
 
     cat > ".env" <<karakeep-env
 KARAKEEP_VERSION=release
@@ -69,9 +88,6 @@ karakeep-env
 
 
 # Copyparty
-
-# nginx proxy manager
-
 
 # overleaf
 if [ -d "$HOME/overleaf" ]; then
@@ -90,10 +106,10 @@ fi
     OVERLEAF_PROJECT_NAME=${input_project:-overleaf-extended}
     read -p "Enter Overleaf App Name (defaults to Overleaf Community Extended): " input_appname
     OVERLEAF_APP_NAME=${input_appname:-Overleaf Community Extended}
-    read -p "Enter Overleaf Port (defaults to 4000): " input_port
-    OVERLEAF_PORT=${input_port:-4000}
-    read -p "Enter Overleaf listening IP (defaults to 127.0.0.1): " input_listening_ip
-    OVERLEAF_LISTEN_IP=${input_listening_ip:-127.0.0.1}
+    read -p "Enter Overleaf Port (defaults to 3000): " input_port
+    OVERLEAF_PORT=${input_port:-3000}
+    read -p "Enter Overleaf listening IP (defaults to 0.0.0.0): " input_listening_ip
+    OVERLEAF_LISTEN_IP=${input_listening_ip:-0.0.0.0}
     read -p "Enter Overleaf site url: " input_url
     OVERLEAF_SITE_URL=${input_url:-localhost://}
 
@@ -143,7 +159,7 @@ services:
     sharelatex:
         image: overleafcep/sharelatex:6.0.1-ext-v3.3
 docker-override
-    sudo ufw allow $OVERLEAF_PORT/tcp
+    sudo ufw allow $OVERLEAF_PORT
     bin/up -d
 )
 
@@ -163,14 +179,28 @@ git clone https://github.com/VERT-sh/VERT $HOME/vert.sh
         --build-arg PUB_DONATION_URL=https://donations.vert.sh \
     	--build-arg PUB_DISABLE_ALL_EXTERNAL_REQUESTS=false \
         --build-arg PUB_STRIPE_KEY="" .
-        docker run -d \
-            --restart unless-stopped \
-            -p 3000:80 \
-            --name "vert" \
-            vert-sh/vert
-
+    docker run -d \
+        --restart unless-stopped \
+        -p 3000:80 \
+        --name "vert" \
+        vert-sh/vert
 )
 # stirling pdf
-
-
+mkdir $HOME/stirling-pdf
+(
+    cd $HOME/stirling-pdf
+    read -p "Enter the port to listen on (default 1000): " stirling_port_input
+    STIRLING_PORT=${stirling_port_input:-1000}
+    cat >docker-compose.yml <<stirling-pdf
+services:
+    stirling-pdf:
+    image: stirlingtools/stirling-pdf:latest-ultra-lite
+    container_name: stirling-pdf
+    ports:
+        - ':8080'
+    volumes:
+        - ./stirling-data:/configs
+    restart: unless-stopped
+stirling-pdf
+)
 # misc. services
