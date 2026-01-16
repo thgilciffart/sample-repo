f55c945681d94664936cd1e658609c224016f944
Jar Jar Binks <wendyscashier1@gmail.com>
2026-01-16 06:09:40 +0000

    Fixed some cd issues

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 # Install yay package maanger on all systems
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git /tmp/yay
-cd /tmp/yay
-makepkg -si
+(
+    cd /tmp/yay
+    makepkg -si
+)
 
 # Install universal packages
 yay -S --needed $(< pkglist.txt)
